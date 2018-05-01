469f26e6314befb1614b43eab2486ff4f34b2dc1
Marcelo Sales <marcelo.d.sales@gmail.com>
2018-05-01 02:30:18 -0300

    update project

--- hola-wildflyswarm/src/main/java/com/redhat/examples/wfswarm/rest/GreeterResource.java
+++ hola-wildflyswarm/src/main/java/com/redhat/examples/wfswarm/rest/GreeterResource.java
@@ -21,7 +21,7 @@ public class GreeterResource {
 	private String backendServiceHost;
 
 	@Inject
-	@ConfigProperty(name = "GREETING_BACKEND_SERVICE_PORT", defaultValue = "8090")
+	@ConfigProperty(name = "GREETING_BACKEND_SERVICE_PORT", defaultValue = "8080")
 	private int backendServicePort;
 
 	@Path("/greeting")
