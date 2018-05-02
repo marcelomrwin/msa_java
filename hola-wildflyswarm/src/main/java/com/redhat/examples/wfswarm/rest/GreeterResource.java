341f30a211dea712acb88b0593d20ae8167c8c0b
Marcelo Sales <marcelo.d.sales@gmail.com>
2018-05-01 21:55:20 -0300

    update project

--- hola-wildflyswarm/src/main/java/com/redhat/examples/wfswarm/rest/GreeterResource.java
+++ hola-wildflyswarm/src/main/java/com/redhat/examples/wfswarm/rest/GreeterResource.java
@@ -1,5 +1,7 @@
 package com.redhat.examples.wfswarm.rest;
 
+import java.util.Arrays;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -8,6 +10,17 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.deltaspike.core.api.config.ConfigProperty;
+
+import com.netflix.client.config.DefaultClientConfigImpl;
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.ILoadBalancer;
+import com.netflix.loadbalancer.LoadBalancerBuilder;
+import com.netflix.loadbalancer.Server;
+import com.netflix.loadbalancer.reactive.LoadBalancerCommand;
+import com.netflix.loadbalancer.reactive.ServerOperation;
+
+import io.fabric8.kubeflix.ribbon.KubernetesServerList;
+import rx.Observable;
 
 @Path("/api")
 public class GreeterResource {
@@ -23,6 +36,25 @@ public class GreeterResource {
 	@Inject
 	@ConfigProperty(name = "GREETING_BACKEND_SERVICE_PORT", defaultValue = "8080")
 	private int backendServicePort;
+
+	private String useKubernetesDiscovery;
+	private ILoadBalancer loadBalancer;
+	private IClientConfig config;
+
+	public GreeterResource() {
+		this.config = new DefaultClientConfigImpl();
+		this.config.loadProperties("backend");
+
+		this.useKubernetesDiscovery = System.getenv("USE_KUBERNETES_DISCOVERY");
+		System.out.println("Value of USE_KUBERNETES_DISCOVERY: " + useKubernetesDiscovery);
+
+		if ("true".equalsIgnoreCase(useKubernetesDiscovery)) {
+			System.out.println("Using Kubernetes discovery for ribbon...");
+			loadBalancer = LoadBalancerBuilder.newBuilder().withDynamicServerList(new KubernetesServerList(config))
+					.buildDynamicServerListLoadBalancer();
+		}
+
+	}
 
 	@Path("/greeting")
 	@GET
@@ -41,6 +73,31 @@ public class GreeterResource {
 
 		BackendCommand command = new BackendCommand(backendServiceHost, backendServicePort).withSaying(saying);
 		BackendDTO backendDTO = command.execute();
+		return backendDTO.getGreeting() + " at host: " + backendDTO.getIp();
+	}
+
+	@Path("/greeting-ribbon")
+	@GET
+	public String greetingRibbon() {
+		if (loadBalancer == null) {
+			System.out.println("Using a static list for ribbon");
+			Server server = new Server(backendServiceHost, backendServicePort);
+			loadBalancer = LoadBalancerBuilder.newBuilder().buildFixedServerListLoadBalancer(Arrays.asList(server));
+		}
+
+		BackendDTO backendDTO = LoadBalancerCommand.<BackendDTO>builder().withLoadBalancer(loadBalancer).build()
+				.submit(new ServerOperation<BackendDTO>() {
+					@Override
+					public Observable<BackendDTO> call(Server server) {
+						String backendServiceUrl = String.format("http://%s:%d", server.getHost(), server.getPort());
+						System.out.println("Sending to: " + backendServiceUrl);
+
+						Client client = ClientBuilder.newClient();
+						return Observable.just(client.target(backendServiceUrl).path("api").path("backend")
+								.queryParam("greeting", saying).request(MediaType.APPLICATION_JSON_TYPE)
+								.get(BackendDTO.class));
+					}
+				}).toBlocking().first();
 		return backendDTO.getGreeting() + " at host: " + backendDTO.getIp();
 	}
 }
